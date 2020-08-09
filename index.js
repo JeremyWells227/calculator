9db8ff1efb4c1e74fe444c7ee6c3e9e863ac3c5c
JeremyWells227 <jswe227@gmail.com>
2020-08-09 19:02:38 -0400

    Backspace Added.  Does not undo multiplication though

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let calculatorStack = [];
 let valueStack = []
 let operatorStack = []
 let currNum="";
+let lastOne;
 let evalDone=false;
 
 const addStack = (item)=>{
@@ -18,6 +19,7 @@ const addStack = (item)=>{
 	}
 	else {
 		currNum+=item.toString()
+		lastOne = "currnum"
 	}
   calculatorStack.push(item);
 	updateDisplay();
@@ -31,6 +33,7 @@ const isOperator = (item) => {
 const addtoPrefixStack = (item)=>{
 	if(typeof(item) === "number") 
 	{
+		lastOne = "value";
 		valueStack.push(item);
 		if (valueStack.length > 1 && (operatorStack[operatorStack.length-1] === "/" || 
 		operatorStack[operatorStack.length-1] === "*") ){
@@ -40,6 +43,7 @@ const addtoPrefixStack = (item)=>{
 	}
 	//string
 	else{
+		lastOne = "operator"
 		if (item === "+" || item === "-") {
 			operatorStack.push(item)
 
@@ -52,22 +56,28 @@ const addtoPrefixStack = (item)=>{
 
 const flushNumBuffer = ()=>
 {
-	if(currNum!==""){
+	if(currNum!=="") {
+		lastOne = "value"
 		let tempNum = currNum;
 		currNum="";
-		addtoPrefixStack(parseInt(tempNum));
+		addtoPrefixStack(parseFloat(tempNum));
 	}
 }
-
+let lastValue1=[];
+let lastValue2=[];
+let lastOp = [];
 const evalOne = () => {
 	let num2 = valueStack.pop();
+	lastValue2.push(num2);
 	let num1 = valueStack.pop();
+	lastValue1.push(num1);
 	if (isNaN(num2)  || isNaN(num1)|| valueStack.length > 100)
 	{
 		alert("Error, invalid input!")
 		return false;
 	}
 	let operator = operatorStack.pop();
+	lastOp.push(operator);
 	let newVal;
 	newVal = operate(num1,num2,operator)
 	valueStack.push(newVal)
@@ -81,17 +91,41 @@ const updateDisplay = () =>{
 		displayText+= item;
 	})
 	document.getElementById("displaybar").innerText = displayText;
+	console.log(`Values ${valueStack}`);
+	console.log(`Currnum ${currNum}`);
+	console.log(`Operators ${operatorStack}`);
+	console.log(`Last One ${lastOne}`)
 }
 
 const clearDisplay = ()=> {
 	calculatorStack = [];
 	updateDisplay();
 }
+
 const clearAll = ()=>{
-	clearDisplay()
+	clearDisplay();
 	valueStack = [];
 	operatorStack = [];
 	currNum="";
+	console.clear();
+}
+
+const backOne = () => {
+	let checktok = lastOne.pop() 
+	if(checktok === 'currnum') {
+		currNum = currNum.slice(0,currNum.length-1);
+	}
+	else if(checktok === 'value') {
+			valueStack.pop()
+	}
+	else if(checktok === 'operator') {
+		operatorStack.pop();
+	}
+	calculatorStack.pop()
+	let newtok = calculatorStack[calculatorStack.length-1]
+	if(typeof(newtok) === "number"){lastOne = "value"}
+	else if(isOperator(tok)){lastOne = "operator"}
+	updateDisplay();
 }
 
 const evalStack =  ()=>{
@@ -109,7 +143,3 @@ const evalStack =  ()=>{
 	updateDisplay();
 	evalDone=true;
 }
-
-
-
-
